c0951fa49cdb2d14c0d964be7612e763de4c3020
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2017-09-08 16:09:14 -0400

    Moved IPC-STT-Notes and IPC-SXX-Note to this repository to prevent redundant postings

---
+++ STT/01-Sep06/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+	printf("Hello world!\nThis is my first program.\n");
+	return 0;
+}
---
+++ SXX/01-Sep06/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+	printf("Hello world again.\nThis is my first program.\n");
+	return 0;
+}
